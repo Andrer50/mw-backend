3c381c9792cd5a7e42d1d8ab0b1e698b3cfee7a4
Andrer50 <sergiorupay990@gmail.com>
2025-10-01 01:02:06 -0500

    mw-backend-004: Se cambió de PostgreSQL a MysqlWorkbench

--- src/main/java/mweb/mw_backend/entity/Product.java
+++ src/main/java/mweb/mw_backend/entity/Product.java
@@ -8,8 +8,8 @@ import lombok.*;
 import mweb.mw_backend.enumeration.ProductCondition;
 import mweb.mw_backend.enumeration.ProductStatus;
 
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 
 @Entity
 @Getter
@@ -31,7 +31,7 @@ public class Product {
     private ProductStatus productStatus;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "condition", nullable = false, length = 20)
+    @Column(name = "product_condition", nullable = false, length = 20)
     private ProductCondition productCondition;
 
     @NotNull(message = "No puede ser nulo")
@@ -52,6 +52,8 @@ public class Product {
     @OneToOne(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private DetailProduct detailProduct;
 
+    // TODO: Descomentar cuando se implementen estas entidades
+    /*
     @OneToOne(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private Inventory inventory;
 
@@ -60,4 +62,5 @@ public class Product {
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Wishlist> wishlists = new ArrayList<>();
+    */
 }
